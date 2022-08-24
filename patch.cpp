e9b66d68b648273e2302e657bf73ab8930ffac77
jzwang <a15923647@gmail.com>
2022-08-24 14:42:09 +0800

    fix idx oob in patch

--- patch.cpp
+++ patch.cpp
@@ -1,25 +1,24 @@
 #include "patch.h"
 
 static inline void insert(std::vector<edit_operation>& opv, size_t& idx, std::string& res) {
-  res += opv[idx++].content;
+  res += opv[idx].content;
 }
 
 static inline void skip(size_t& x_idx, std::vector<edit_operation>& opv, size_t& idx) {
-  x_idx += opv[idx++].content.length();
+  x_idx += opv[idx].content.length();
 }
 
 void Patch::patch(std::string& x, std::vector<edit_operation>& opv, std::string& res) {
   size_t idx = 0;
   for (size_t x_idx = 0; idx < opv.size() || x_idx < x.length(); x_idx++) {
-    while (x_idx == opv[idx].pos) {
+    while (idx < opv.size() && x_idx == opv[idx].pos) {
       //opv[idx].show();
       if (opv[idx].op == EditOperation::Insert) {
-        //res += opv[idx++].content;
         insert(opv, idx, res);
       } else if (opv[idx].op == EditOperation::Delete) { //Delete
-        //x_idx += opv[idx++].content.length();
         skip(x_idx, opv, idx);
       }
+      idx++;
     }
     if (x_idx < x.length()) res += x[x_idx];
   }
